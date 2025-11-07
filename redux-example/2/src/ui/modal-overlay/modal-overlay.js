5ca645de8b72e9cc4ec4c3362ba4ff5c1c557f86
Alexey Shcheglov <a.shcheglov@unlimint.com>
2025-11-08 00:51:04 +0400

    Удалены лишние файлы

--- redux-example/2/src/ui/modal-overlay/modal-overlay.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './modal-overlay.module.css';
-
-export const ModalOverlay = ({extraClass}) => {
-    return <div className={`${styles.overlay} ${extraClass}`}/>;
-};
